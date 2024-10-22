07861f79801f8614ed37bbfc499a97cbbd8e785e
Isabela Iunes <iunesisabela.cs@gmail.com>
2024-10-21 21:57:58 -0300

    master

--- lista.py
+++ lista.py
@@ -10,10 +10,10 @@ for num in numeros:
 
 # Pode-se fazer isso através de uma list comprehension também #
 
-#Criar uma lista com os números entre 1 e 100 #
+# Criar uma lista com os números entre 1 e 100 #
 numeros = list(range(1, 101))
 
-#Usa a list comprehension para gerar uma lista somente com os números pares e divisíveis por 4 #
+# Usa a list comprehension para gerar uma lista somente com os números pares e divisíveis por 4 #
 pares_div = [num for numero in numeros if numero % 2 == 0 and numero % 4 == 0] 
 
 # Imprima a lista gerada #
