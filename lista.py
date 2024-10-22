6f8857177d38a49c2e8c38763c3ab55dd655a38e
Isabela Iunes <iunesisabela.cs@gmail.com>
2024-10-21 21:55:00 -0300

    master

--- lista.py
+++ lista.py
@@ -1,7 +1,20 @@
-# Criar uma lista de números de 1 a 100
+# Criar uma lista de números de 1 a 100 #
 numeros = list(range(1, 101))
+
 # Imprimir os números pares que são divisíveis por 4 #
 for num in numeros:
     if num % 2 == 0 and num % 4 == 0:
         print(num)
-        
+
+
+
+# Pode-se fazer isso através de uma list comprehension também #
+
+#Criar uma lista com os números entre 1 e 100 #
+numeros = list(range(1, 101))
+
+#Usa a list comprehension para gerar uma lista somente com os números pares e divisíveis por 4 #
+pares_div = [num for numero in numeros if numero % 2 == 0 and numero % 4 == 0] 
+
+# Imprima a lista gerada #
+print(pares_div4)
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 Este é o projeto 100Par4. Ao contrário do que se pode ler, ele para sim.
-Afinal consiste em um código Python para gerar uma lista com números de 1 a 100 que são pares divisíveis por 4.
+Afinal consiste em um código Python para gerar uma lista com números entre 1 e 100 que são pares divisíveis por 4.
